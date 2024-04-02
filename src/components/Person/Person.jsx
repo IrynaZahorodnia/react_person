48ce8574e54e5390844ac74cbdea75b83bf2892f
Iryna Zahorodnia <irishas25@ukr.net>
2024-04-02 19:06:40 +0300

    destructure person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,17 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age && <p className="Person__age">I am {person.age}</p>}
-    <p className="Person__partner">
-      {person.isMarried
-        ? `${person.partnerName} is my ${person.sex === 'm' ? `wife` : `husband`}`
-        : `I am not married`}
-    </p>
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const personPartner = sex === 'm' ? `wife` : `husband`;
+  const personMarriedStatus = isMarried
+    ? `${partnerName} is my ${personPartner}`
+    : `I am not married`;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {age && <p className="Person__age">I am {age}</p>}
+
+      <p className="Person__partner">{personMarriedStatus}</p>
+    </section>
+  );
+};
